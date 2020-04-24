369202c24d066652232d748f07df399f02a03a24
Mark Rodgers <mark.rodgers15@pcc.edu>
2020-04-24 14:51:09 -0700

    Added Comments and personalized name display

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
-
+/**
+ * CIS-233J
+ * This is a simple "Hello World" application to exercise features in GitBash and GitHub
+ * @author Mark Rodgers
+ * @version 2020.4.24
+ */
 public class Main {
 
     public static void main(String[] args) {
+	String firstName = "Mark";
 	System.out.println("Hello World!");
+	System.out.println("And a special hello to you, " + firstName + "!");
     }
 }
